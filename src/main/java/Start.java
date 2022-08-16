2afe086fcf3c5a59d8d187a8921b75ecb58f40d0
Stepan Polyakov <polyakovs@list.ru>
2022-08-16 17:08:25 +0700

    integer sorted with Arraylist

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -3,6 +3,8 @@ import repository.SortInteger;
 public class Start {
     public static void main(String[] args) {
         SortInteger sortInteger = new SortInteger();
+        sortInteger.getA1();
+        sortInteger.getA2();
         sortInteger.sorted();
 
     }
--- src/main/java/repository/SortInteger.java
+++ src/main/java/repository/SortInteger.java
@@ -1,27 +1,61 @@
 package repository;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 
-import static java.lang.System.arraycopy;
 
 public class SortInteger {
-    int[] a1 = new int[]{1,3,5,10,11,25};
-    int[] a2 = new int[]{2,4,1,9,3,22};
-    int[] a3 = new int[a1.length + a2.length];
-    int i=0, j=0, k = 0, b =0, a;
-    public void sorted() {
-        while (i < a1.length && j < a2.length) {
-            a = a1[i] < a2[j] ? a1[i++] : a2[j++];
-            if (a > b) {
-                a3[k++] = a;
-            }
-            b = a;
-        }
-        if (i < a1.length) {
-            arraycopy(a1, i, a3, k, a1.length - i);
-        } else if (j < a2.length) {
-            arraycopy(a2, j, a3, k, a2.length - j);
-        }
-        System.out.println(Arrays.toString(a3));
+    ArrayList<Integer> a1 = new ArrayList<>();
+
+    public ArrayList<Integer> getA1() {
+        a1.add(1);
+        a1.add(5);
+        a1.add(10);
+
+
+        return a2;
     }
-}
+
+    ArrayList<Integer> a2 = new ArrayList<>();
+
+    public ArrayList<Integer> getA2() {
+        a2.add(3);
+        a2.add(5);
+        a2.add(11);
+        a2.add(15);
+        a2.add(111);
+        a2.add(26);
+        return a2;
+    }
+
+    ArrayList<Integer> a3 = new ArrayList<>();
+    int i = 0, j = 0, b = 0, a;
+
+    public void sorted() {
+
+        while (i < a1.size() && j < a2.size()) {
+            a = a1.get(i) < a2.get(j) ? a1.get(i++) : a2.get(j++);
+            if (a >= b) {
+                a3.add(a);
+                b = a3.get(a3.size() - 1);
+            }
+
+        }
+        while (i < a1.size()) {
+            if (a1.get(i) > a3.get(a3.size() - 1)) {
+                a3.add(a1.get(i++));
+            }
+            else {
+                i++;
+            }
+        }
+        while (j < a2.size()) {
+            if (a2.get(j) > a3.get(a3.size() - 1)) {
+                a3.add(a2.get(j++));
+            }
+            else {
+                j++;
+            }
+        }
+            System.out.println(a3);
+        }
+    }
